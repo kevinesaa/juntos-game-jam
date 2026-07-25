c4ac134027c57b65c4905e5d0d956023a0bba717
Kevin Esaa <kevin.esaa@gmail.com>
2026-07-25 15:07:17 -0400

    feat: skill change

--- player-controller/MyCharacterController.gd
+++ player-controller/MyCharacterController.gd
@@ -5,8 +5,10 @@ extends Node2D
 @export var baseSpeed:float
 @export var skills_node_paths:Array[NodePath] =[]
 
-
-var indexId:int
+#region skill controller signals
+signal on_current_skill_change(characterIndexId:int, skillIndex:int)
+#endregion
+var characterIndexId:int
 var newPosition:Vector2 = Vector2.ZERO
 var currentSkillIndex:int = 0
 var charaterSkills: Array[SkillController] = []
@@ -16,12 +18,11 @@ func _ready() -> void:
 	for path in skills_node_paths:
 		var skillNode = get_node_or_null(path)
 		var skill = skillNode as SkillController
-		print(skill)
 		charaterSkills.append(skill)
 	
 
-func setIndexId(id:int) -> void:
-	self.indexId = id
+func setCharacterIndexId(id:int) -> void:
+	self.characterIndexId = id
 
 func moveCharacter(deltaTime:float,moveVector:Vector2) -> void:
 	
@@ -29,8 +30,19 @@ func moveCharacter(deltaTime:float,moveVector:Vector2) -> void:
 	var position:Vector2 = self.position + self.newPosition
 	self.position = position
 
-func selectNextSkill() -> void:
-	pass
-	
 func selectPreviusSkill() -> void:
-	pass
+	
+	self.currentSkillIndex = self.currentSkillIndex - 1
+	if(self.currentSkillIndex < 0):
+		self.currentSkillIndex = self.charaterSkills.size() - 1
+	
+	on_current_skill_change.emit(self.characterIndexId,self.currentSkillIndex)
+	
+func selectNextSkill() -> void:
+	
+	self.currentSkillIndex = self.currentSkillIndex + 1
+	var maxIndex:int = charaterSkills.size() - 1
+	if(self.currentSkillIndex > maxIndex):
+		self.currentSkillIndex = 0
+	
+	on_current_skill_change.emit(self.characterIndexId,self.currentSkillIndex)
--- player-controller/PlayerController.gd
+++ player-controller/PlayerController.gd
@@ -8,6 +8,11 @@ extends Node
 
 signal on_press_pause_notify(pauseStatus:bool)
 
+#region player signal
+signal on_current_character_change(indexId:int)
+signal on_selected_skill_change_notify(characterIndexId:int, skillIndexId:int)
+#endregion
+
 var pauseStatus:bool = false
 var characters:Array[MyCharacterController] = []
 var currentSelectedIndex:int = 0
@@ -21,7 +26,8 @@ func _ready() -> void:
 	
 	for i in characters.size():
 		var c = characters.get(i)
-		c.setIndexId(i)
+		c.setCharacterIndexId(i)
+		c.on_current_skill_change.connect(self._selectedSkillChangeListener)
 		
 	
 func  _process(delta: float) -> void:
@@ -42,9 +48,16 @@ func  _process(delta: float) -> void:
 	if(nextCharacter):
 		_nextCharacter()
 	
-	
+	var previusSkill = Input.is_action_just_pressed("previus_skill")
+	var nextSkill = Input.is_action_just_pressed("next_skill")
 	var moveInput:float = Input.get_axis("move_left","move_right")
 	
+	if(previusSkill):
+		self._previusSkill()
+	
+	if(nextSkill):
+		self._nextSkill()
+		
 	_move(delta,moveInput)
 	
 	
@@ -57,17 +70,31 @@ func  _previusCharater() -> void:
 	self.currentSelectedIndex = self.currentSelectedIndex - 1
 	if(self.currentSelectedIndex < 0):
 		self.currentSelectedIndex = self.characters.size() - 1
-		
+	
+	on_current_character_change.emit(self.currentSelectedIndex)		
+	
 func _nextCharacter() -> void:
 	self.currentSelectedIndex = self.currentSelectedIndex + 1
 	var maxIndex:int = characters.size() - 1
 	if(self.currentSelectedIndex > maxIndex):
 		self.currentSelectedIndex = 0
 	
+	on_current_character_change.emit(self.currentSelectedIndex)
+	
+	
 func _move(deltaTime:float, moveInput:float) -> void:
 	var currentCharacter:MyCharacterController = self.characters.get(self.currentSelectedIndex)
 	var moveVector:Vector2 = moveInput * Vector2.RIGHT
 	currentCharacter.moveCharacter(deltaTime,moveVector)
 	
+func _previusSkill() -> void:
+	var currentCharacter:MyCharacterController = self.characters.get(self.currentSelectedIndex)
+	currentCharacter.selectPreviusSkill()
 	
+func _nextSkill() -> void:
+	var currentCharacter:MyCharacterController = self.characters.get(self.currentSelectedIndex)
+	currentCharacter.selectNextSkill()
 	
+func _selectedSkillChangeListener(characterIndexId:int, skillIndexId:int) -> void:
+	
+	self.on_selected_skill_change_notify.emit(characterIndexId,skillIndexId)
