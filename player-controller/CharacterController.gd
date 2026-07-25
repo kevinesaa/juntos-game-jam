19d69faf744e5f3878095ee44dd08d5046029608
Kevin Esaa <kevin.esaa@gmail.com>
2026-07-24 22:24:54 -0400

    feat: change characters

---
+++ player-controller/CharacterController.gd
@@ -0,0 +1,2 @@
+class_name CharacterController
+extends Node
---
+++ player-controller/PlayerController.gd
@@ -0,0 +1,48 @@
+class_name PlayerController
+extends Node
+
+@onready var player_1_node_2d: CharacterController = $player1_Node2D
+@onready var player_2_node_2d: CharacterController = $player2_Node2D
+@onready var player_3_node_2d: CharacterController = $player3_Node2D
+@onready var player_4_node_2d: CharacterController = $player4_Node2D
+
+var characters:Array[ChangeSceneController] = []
+var currentSelectedIndex:int = 0
+
+
+func _init() -> void:
+	characters.append(player_1_node_2d)
+	characters.append(player_2_node_2d)
+	characters.append(player_3_node_2d)
+	characters.append(player_4_node_2d)
+
+
+func _ready() -> void:
+	pass
+	
+	
+func  _process(delta: float) -> void:
+	
+	var previusCharacter = Input.is_action_just_pressed("previus_character")
+	var nextCharacter = Input.is_action_just_pressed("next_character")
+	
+	if(previusCharacter):
+		_previusCharater()
+	if(nextCharacter):
+		_nextCharacter()
+
+func  _previusCharater() -> void:
+	self.currentSelectedIndex = self.currentSelectedIndex - 1
+	if(self.currentSelectedIndex < 0):
+		self.currentSelectedIndex = self.characters.size() - 1
+	
+	print(self.currentSelectedIndex)
+	
+func _nextCharacter() -> void:
+	self.currentSelectedIndex = self.currentSelectedIndex + 1
+	var maxIndex:int = characters.size() - 1
+	if(self.currentSelectedIndex > maxIndex):
+		self.currentSelectedIndex = 0
+	
+	print(self.currentSelectedIndex)
+	
